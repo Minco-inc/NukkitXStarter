f5f325c8550947a8d5c4bd772b30b6303cbc4df8
Hen2527 <hen2527@outlook.com>
2021-09-07 14:10:54 +0800

    NukkitX Linux Shell Script
    Java 8 is required! Download Java -> https://java.com/en/download

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
+#!/bin/bash
 echo =============================================
 echo NukkitX Starter for Minecraft Bedrock Edition
 echo =============================================
-echo
-sleep 5
+echo                                              
 echo =============================================
 echo Starting NukkitX 1.0...
 echo =============================================
-if [ -s nukkit.jar ]
- then
- java -jar nukkit.jar
- else                                             
-echo "Nukkit.jar is Not found!"
-echo "Downloads are on https://github.com/CloudburstMC/Nukkit/"
+if [ -e Nukkit.jar ];
+then 
+    java -jar nukkit.jar
+else                                         
+    echo "Nukkit.jar is Not found!"
+    echo "Downloads are on https://github.com/CloudburstMC/Nukkit/"
 fi
