2ac370c393430df9d65aa66b27f90a2a8b7a50ca
Hen2527 <hen2527@outlook.com>
2021-09-07 10:37:03 +0800

    For Windows Only
    Removed Sleep Powershell command

--- start.bat
+++ start.bat
@@ -22,15 +22,20 @@ TITLE NukkitX-1.0 : A Minecraft Bedrock Edition Software
 echo ===================================================
 echo NukkitX Starter for Minecraft Bedrock Edition Software
 echo ===================================================
-powershell -command "Start-Sleep -s 1"
 echo.
 echo ===================================================
 echo Starting...
 echo ===================================================
-powershell -command "Start-Sleep -s 1"
 if exist nukkit.jar (
+echo ===================================================
+echo Found Nukkit.jar! Starting...
+echo ===================================================
      java -jar nukkit.jar
 ) else (
+echo ===================================================
 echo Nukkit.jar is Not found!
 echo Downloads are on https://github.com/CloudburstMC/Nukkit/releases
+echo OR
+echo Rename your nukkit jar file to 'nukkit.jar'
+echo ===================================================
 )
